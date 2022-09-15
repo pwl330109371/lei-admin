3c239b50703d1191ff2996ebd097b831c9c095fc
pengwenlei <pengwenlei@pengwenlei>
2022-09-15 14:44:57 +0800

    feat(代码格式化): 项目初始化，删除无用的代码

--- .history/src/main_20220914111953.js
+++ .history/src/main_20220914111953.js
@@ -3,5 +3,4 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-const app = createApp(App)
-app.use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
